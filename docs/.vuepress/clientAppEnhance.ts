e8a04b2ea26f45171bb35467aecfc85a6b388fe7
d_etteiu8383 <ysdr83@gmail.com>
2022-04-26 02:53:34 +0900

    Youtube埋め込みの追加

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -1,7 +1,9 @@
 import { defineClientAppEnhance } from '@vuepress/client'
+import YouTube from 'vue3-youtube'
 import Tweet from "vue-tweet";
 
 export default defineClientAppEnhance(({ app }) => {
   // set global components
+  app.component('YouTube', YouTube)
   app.component('Tweet', Tweet)
 })
